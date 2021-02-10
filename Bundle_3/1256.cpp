d80562b4bdcbc671a27b1e39fadc3674347e843a
hwchu <foxytasarin@gmail.com>
2021-02-10 17:26:13 +0900

    Update 1256.cpp
    Solved

--- Bundle_3/1256.cpp
+++ Bundle_3/1256.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
 #include <queue>
 
-int factorial(int n){
-    return (n == 0) || (n == 1) ? 1 : n * factorial(n-1);
-}
+#define MAX 1000000000
 
 int main(){
 
-    int N, M, K;
+    long long N, M, K;
 
     std::cin >> N >> M >> K;
 
-    int astack = N;
-    int zstack = M;
+    // a, z가지고 만들 수 있는 문자열 개수를 배열에 저장
+    long long select[101][101];
 
-    int startpos = 1;
-    int endpos = factorial(astack+zstack) / (factorial(astack) * factorial(zstack));
+    for(long long i=0; i<=N; i++){
+        for(long long j=0; j<=M; j++){
+            if(i == 0){
+                select[i][j] = 1;
+            }
+            else if(j == 0){
+                select[i][j] = 1;
+            }
+            else{
+                if(select[i][j-1]*(i+j) / j > MAX){
+                    select[i][j] = MAX;
+                }
+                else{
+                    select[i][j] = select[i][j-1]*(i+j) / j;
+                }
+            }
+        }
+    }
 
-    if(K > endpos){
+    if(K > select[N][M]){
         std::cout << -1;
         return 0;
     }
 
     std::queue<char> q;
 
-    // startpos or endpos == K 가 될때까지
+    long long astack = N;
+    long long zstack = M;
+
     while(1){
         if(astack == 0){
             break;
@@ -32,22 +48,17 @@ int main(){
         if(zstack == 0){
             break;
         }
-        long long aselect = factorial(astack-1+zstack) / (factorial(astack-1) * factorial(zstack));
-        long long zselect = factorial(astack+zstack-1) / (factorial(astack) * factorial(zstack-1));
 
-        // a 선택하고 남은 경우의 수의 시작지점이 K보다 크다면 a 선택하는게 맞지
-        if(startpos + aselect - 1 >= K){
-            astack -= 1;
+        // a 선택하고 남은 경우의 수가 K보다 크다면 a 선택하는게 맞지
+        if(select[astack-1][zstack] >= K){
             q.push('a');
-            // 그리고 endpos를 변경
-            endpos = aselect;
+            astack -= 1;
         }
-        // 그게 아니면 z를 선택해야함. 시작지점이 K보다 작거나 같기때문에
+        // 그게 아니면 z를 선택해야함. 경우의 수가 K보다 작거나 같기때문에
         else{
-            zstack -= 1;
             q.push('z');
-            // 그리고 startpos를 변경
-            startpos = startpos + aselect;
+            K -= select[astack-1][zstack];
+            zstack -= 1;
         }
     }
 
@@ -56,7 +67,7 @@ int main(){
         astack -= 1;
     }
 
-     while(zstack > 0){
+    while(zstack > 0){
         q.push('z');
         zstack -= 1;
     }
@@ -65,7 +76,6 @@ int main(){
         std::cout << q.front();
         q.pop();
     }
-
 
     return 0;
 }
