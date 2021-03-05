19d2c72140ca5e6bd6f6df6ae9082f5e8f431038
hwchu <foxytasarin@gmail.com>
2021-03-05 19:02:36 +0900

    Update 15686.cpp
    Solved

--- Bundle_4/15686.cpp
+++ Bundle_4/15686.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstdlib>
 
 int N;
 int M;
@@ -47,7 +49,6 @@ void dfs(int start, int count){
         choose[i] = 0;
         // dfs(start+1, count+1);
     }
-
 }
 
 int main(){
@@ -70,7 +71,42 @@ int main(){
         }
     }
 
-    dfs(0, 0);
+    // next_permutation을 사용한 combination 구하기.
+    std::vector<int> filter;
+
+    // nCm에서 m개만큼 1을 넣고 아닌것 만큼 0 넣기
+    for(int i=0; i<M; i++){
+        filter.push_back(1);
+    }
+    for(int i=0; i<chicken.size()-M; i++){
+        filter.push_back(0);
+    }
+    std::sort(filter.begin(), filter.end());
+
+    do{
+        int answer = 0;
+        for(int i=0; i<house.size(); i++){
+            int min_distance = 987654321;
+
+            for(int j=0; j<filter.size(); j++){
+                if(filter[j] == 1){
+                    int distance = std::abs(house[i].first - chicken[j].first) + std::abs(house[i].second - chicken[j].second);
+
+                    if(min_distance > distance){
+                        min_distance = distance;
+                    }
+                }
+            }
+
+            answer += min_distance;
+        }
+
+        if(final_answer > answer){
+            final_answer = answer;
+        }
+    }while(std::next_permutation(filter.begin(), filter.end()));
+
+    // dfs(0, 0);
 
     std::cout << final_answer;
 
