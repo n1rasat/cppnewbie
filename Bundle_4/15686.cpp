3d72fad39fa9172055b45f75e2edfb047f4ce3ca
hwchu <foxytasarin@gmail.com>
2021-03-05 19:10:36 +0900

    Update 15686.cpp
    Delete unused part

--- Bundle_4/15686.cpp
+++ Bundle_4/15686.cpp
@@ -6,50 +6,10 @@
 int N;
 int M;
 
-int graph[51][51] = {0, };
-
 std::vector<std::pair<int, int>> house;
 std::vector<std::pair<int, int>> chicken;
 
-int choose[14] = {0, };
-
 int final_answer = 987654321;
-
-void dfs(int start, int count){
-    if(count == M){
-        int answer = 0;
-
-        for(int i=0; i<house.size(); i++){
-            int min_distance = 987654321;
-            // 각 집의 치킨거리의 최소값
-            for(int j=0; j<chicken.size(); j++){
-                if(choose[j] == 1){
-                    int distance = std::abs(house[i].first - chicken[j].first) + std::abs(house[i].second - chicken[j].second);
-                    if(min_distance > distance){
-                        min_distance = distance;
-                    }
-                }
-            }
-            answer += min_distance;
-        }
-
-        // 치킨거리 합의 최소값
-        if(final_answer > answer){
-            final_answer = answer;
-        }
-
-        return;
-    }
-
-    // backtracking
-    // chicken vector에서 combination M 개. i번째 치킨집 
-    for(int i=start; i<chicken.size(); i++){
-        choose[i] = 1;
-        dfs(start+1, count+1);
-        choose[i] = 0;
-        // dfs(start+1, count+1);
-    }
-}
 
 int main(){
 
@@ -59,8 +19,6 @@ int main(){
         for(int j=1; j<=N; j++){
             int x;
             scanf("%d", &x);
-            graph[i][j] = x;   
-
             if(x == 1){
                 house.push_back(std::make_pair(i, j));
 
@@ -105,8 +63,6 @@ int main(){
             final_answer = answer;
         }
     }while(std::next_permutation(filter.begin(), filter.end()));
-
-    // dfs(0, 0);
 
     std::cout << final_answer;
 
