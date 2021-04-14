af70d07891805e70b65dc83ccd28bbbd08c61cc1
knudeallab <foxytasarin@gmail.com>
2021-04-14 19:25:29 +0900

    Update 17471.cpp
    Using next_permutation

--- Bundle_04/17471.cpp
+++ Bundle_04/17471.cpp
@@ -1,113 +1,133 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <string.h>
 #include <algorithm>
+#include <queue>
+#include <cmath>
 
-int graph[11][11];
-int people[11];
-int region[11];
+using namespace std;
 
+int map[11][11] = {0, };
+int people[11] = {0, };
+int visited[11] = {0, };
 int N;
-
 int answer = 987654321;
 
-int bfs(std::vector<int>& region){
-    int visited[11] = {0, };
-    std::queue<int> q;
+int bfs(vector<int> land){
+    queue<int> q;
 
-    visited[region[0]] = 1;
-
-    q.push(region[0]);
+    q.push(land[0]);
+    visited[land[0]] = 1;
 
     while(!q.empty()){
         int temp = q.front();
+
         q.pop();
 
         for(int i=1; i<=N; i++){
-            // 연결된 vertex가 있으면
-            if(graph[temp][i] == 1){
-                if(visited[i] == 1){
-                    continue;
-                }
-                
-                if(std::binary_search(region.begin(), region.end(), i)){
-                    visited[i] = 1;
-                    q.push(i);
-                }
+            if(map[temp][i] == 0)
+                continue;
+
+            if(visited[i] == 1)
+                continue;
+
+            if(binary_search(land.begin(), land.end(), i)){
+                visited[i] = 1;
+                q.push(i);
             }
+            
         }
     }
-    for(int i=0; i<region.size(); i++){
-        if(visited[region[i]] == 0){
-            return 0;
+
+    for(int i=0; i<land.size(); i++){
+        if(visited[land[i]] == 0){
+            return 1;
         }
     }
-    
-    return 1;
+
+    return 0;
 }
 
 int main(){
-    std::cin >> N;
+    cin >> N;
 
     for(int i=1; i<=N; i++){
-        int x;
-        std::cin >> x;
-        people[i] = x;
+        cin >> people[i];
     }
 
-    // 선거구 수 만큼
     for(int i=1; i<=N; i++){
         int x;
-        std::cin >> x;
+        cin >> x;
         for(int j=0; j<x; j++){
             int y;
-            std::cin >>y;
-            
-            // make graph
-            graph[i][y] = 1;
-            graph[y][i] = 1;
+            cin >> y;
+            map[i][y] = 1;
+            map[y][i] = 1;
         }
     }
+    
+    int perm = 1;
 
-    // bitmask로 combination
-    for(int bit=1; bit < (1 << N)-1; bit++){
-        std::vector<int> red, blue;
-        for(int i=0; i<N; i++){
-            // bit == 1: red
-            // bit == 0: blue
-            if(bit & 1 << i){
-                red.push_back(i+1);
-            }
-            else{
-                blue.push_back(i+1);
-            }
+    while(1){
+        if(perm > N/2)
+            break;
+
+        vector<int> filter;
+
+        for(int i=0; i<perm; i++){
+            filter.push_back(1);
+        }
+        for(int i=perm; i<N; i++){
+            filter.push_back(0);
         }
 
-        if(bfs(red) && bfs(blue)){
-            int diff = 0;
+        sort(filter.begin(), filter.end());
 
-            for(int i=0; i<red.size(); i++){
-                diff += people[red[i]];
+        do{
+            // 1: red; 0: blue;
+            vector<int> red;
+            vector<int> blue;
+            for(int i=0; i<N; i++){
+                if(filter[i] == 1){
+                    red.push_back(i+1);
+                }
+                else{
+                    blue.push_back(i+1);
+                }
             }
-            
-            for(int i=0; i<blue.size(); i++){
-                diff -= people[blue[i]];
-            }
-            diff = abs(diff);
 
-            if(answer > diff){
-                answer = diff;
+            int red_check = bfs(red);
+            memset(visited, 0, sizeof(visited));
+            int blue_check = bfs(blue);
+            memset(visited, 0, sizeof(visited));
+
+            if(red_check == 0 && blue_check == 0){
+                int tempans = 0;
+                for(int i=0; i<red.size(); i++){
+                    tempans += people[red[i]];
+                }
+                for(int i=0; i<blue.size(); i++){
+                    tempans -= people[blue[i]];
+                }
+                tempans = abs(tempans);
+
+                if(tempans < answer)
+                    answer = tempans;
             }
-        }
+
+        }while(next_permutation(filter.begin(), filter.end()));
+
+        perm += 1;
     }
 
     if(answer == 987654321){
-        std::cout << -1;
-        return 0;
+        cout << -1;
+
+    }
+    else{
+        cout << answer;
     }
 
-    std::cout << answer;
-
-
     return 0;
+
 }
